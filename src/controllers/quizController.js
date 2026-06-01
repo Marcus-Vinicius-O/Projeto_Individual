bbe250a1dd24db36b76c350efbc6f2dfa10810ca
Marcus-Vinicius-O <marcus.osilva@sptech.school>
2026-06-01 15:46:02 -0300

    atualizações projeto

--- src/controllers/quizController.js
+++ src/controllers/quizController.js
@@ -2,10 +2,10 @@ const quizModel = require('../models/quizModel');
 
 function listarPerguntas(req, res) {
     quizModel.listarPerguntas()
-        .then(function(resultado) {
+        .then(function (resultado) {
             res.status(200).json(resultado);
         })
-        .catch(function(erro) {
+        .catch(function (erro) {
             console.log(erro);
             res.status(500).json({ erro: "Erro ao buscar perguntas." });
         });
@@ -14,19 +14,20 @@ function salvarRespostas(req, res) {
     const { fk_usuario, respostas, fk_personagem_resultado } = req.body;
 
     quizModel.salvarTentativa(fk_usuario, fk_personagem_resultado)
-        .then(function(resultado) {
+        .then(function (resultado) {
             const fk_tentativa = resultado.insertId;
 
-            const promessas = respostas.map(function(resposta) {
-                return quizModel.salvarResposta(fk_tentativa, resposta.fk_pergunta, resposta.fk_alternativa);
-            });
+            const promessas = [];
+            for (let i = 0; i < respostas.length; i++) {
+                promessas.push(quizModel.salvarResposta(fk_tentativa, respostas[i].fk_pergunta, respostas[i].fk_alternativa));
+            }
 
             return Promise.all(promessas);
         })
-        .then(function() {
+        .then(function () {
             res.status(201).json({ mensagem: "Respostas salvas com sucesso!" });
         })
-        .catch(function(erro) {
+        .catch(function (erro) {
             console.log(erro);
             res.status(500).json({ erro: "Erro ao salvar respostas." });
         });
@@ -35,14 +36,14 @@ function buscarResultado(req, res) {
     const { fk_usuario } = req.params;
 
     quizModel.buscarResultado(fk_usuario)
-        .then(function(resultado) {
+        .then(function (resultado) {
             if (resultado.length === 0) {
                 res.status(404).json({ mensagem: "Nenhum resultado encontrado." });
             } else {
                 res.status(200).json(resultado[0]);
             }
         })
-        .catch(function(erro) {
+        .catch(function (erro) {
             console.log(erro);
             res.status(500).json({ erro: "Erro ao buscar resultado." });
         });
